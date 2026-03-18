60e9b3b77e22763d507bf7ae8852839239bf8499
Elle Christine Ramstad <ellram@eduroam-193-157-203-169.wlan.uio.no>
2026-03-18 15:19:54 +0100

    Added -test open new account creates new account successfully- as testcae 02

--- tests/test_banking_operations.spec.ts
+++ tests/test_banking_operations.spec.ts
@@ -1,3 +1,8 @@
+/**
+ * Run npx:
+ * npx playwright test test_banking_operations.spec.ts
+ */
+
 import { test, expect } from '@playwright/test';
 import { executionAsyncId } from 'node:async_hooks';
 
@@ -8,7 +13,7 @@ test.describe('Testing suite for testing tree different functionallities from we
         await page.fill('input[name="username"]', 'john');
         await page.fill('input[name="password"]', 'demo');
         await page.locator('input[type="submit"][value="Log In"]').click(); 
-        await page.waitForTimeout(2000); // venter 2 sekunder så jeg rekker å se hva som skjer
+        //await page.waitForTimeout(2000); // venter 2 sekunder så jeg rekker å se hva som skjer
 
     });
 
@@ -18,8 +23,22 @@ test.describe('Testing suite for testing tree different functionallities from we
     });
 
     test('test open new account creates new accoun successfully', async ({ page }) => {
-        await page.locator('ul.button li.contact a').click();    
+
+        //trykk på riktige felter
+        await page.locator('li >> text=Open New Account').click();
+        await page.locator('select#type').selectOption({ label: 'SAVINGS' });
+        await page.locator('select#fromAccountId').selectOption({ label: '12789' }); //denne kontostringen må byttes ut basert på hvilke kontoer som finnes i databasen på tidspunktet av testingen
+        await page.locator('input[type="button"][value="Open New Account"]').click(); 
+
+        //bekreft på side
+        const message = page.locator('#openAccountResult p', { hasText: 'Congratulations' });
+        await expect(message).toHaveCount(1);    
+        const accountLink = page.locator('#newAccountId');
+        await expect(accountLink).toHaveCount(1);
+
+        await page.waitForTimeout(2000) //bare for testing
+
     });
 }); 
 
-//npx playwright test test_banking_operations.spec.ts
+
