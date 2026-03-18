c5b3d2f22e4e8215a543270d224106ea199b0ab6
Elle Christine Ramstad <ellram@eduroam-193-157-203-169.wlan.uio.no>
2026-03-18 11:20:14 +0100

    Added code for making it a test suite, added description

--- tests/customer_care_test.spec.ts
+++ tests/customer_care_test.spec.ts
@@ -1,34 +1,31 @@
 import { test, expect } from '@playwright/test';
 
-test('has link', async ({ page }) => {
-  await page.goto('https://parabank.parasoft.com/parabank/index.htm');
-  await expect(page).toHaveTitle(/ParaBank/);
+test.describe('Customer Care Tests', () => {
+
+  test('has link', async ({ page }) => {
+    await page.goto('https://parabank.parasoft.com/parabank/index.htm');
+    await expect(page).toHaveTitle(/ParaBank/);
+  });
+
+  test('go to customer care', async ({ page }) => {
+    await page.goto('https://parabank.parasoft.com/parabank/index.htm');
+    await page.locator('ul.button li.contact a').click();
+  });
+
+  test('fill inn information in fields', async ({ page }) => {
+    await page.goto('https://parabank.parasoft.com/parabank/index.htm');
+    await page.locator('ul.button li.contact a').click();
+
+    await page.fill('#name', 'Quorra T. Flynn');
+    await page.fill('#email', 'quorra@example.com');
+    await page.fill('#phone', '12345678');
+    await page.fill('#message', 'This is a test message.');
+
+    await page.click('#contactForm input[type="submit"]');
+    const messages = page.locator('#rightPanel p');
+    await expect(messages).toHaveCount(2); 
+  });
 });
-
-test('go to customer care', async ({ page }) => {
-  await page.goto('https://parabank.parasoft.com/parabank/index.htm');
-  await page.locator('ul.button li.contact a').click();
-});
-
-test('fill inn information in fields', async ({ page }) => {
-  await page.goto('https://parabank.parasoft.com/parabank/index.htm');
-  await page.locator('ul.button li.contact a').click()
-  
-  await page.fill('#name', 'Quorra T. Flynn');
-  await page.fill('#email', 'quorra@example.com');
-  await page.fill('#phone', '12345678');
-  await page.fill('#message', 'This is a test message.');
-  
-  // for å se testing, bruk denne:
-  await page.pause();
-  
-  await page.click('#contactForm input[type="submit"]');
-
-  const messages = page.locator('#rightPanel p');
-  await expect(messages).toHaveCount(2);
-});
-
-
 /* 
 For running tests without view:
 npx playwright test customer_care_test.spec.ts
